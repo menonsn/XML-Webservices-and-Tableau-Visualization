c705030ce739f4dd70c83f5eebc54ad50e3bdece
Aratil <56776378+Aratil@users.noreply.github.com>
2019-11-16 21:48:12 -0500

    My changes are: •	Refactor duplicate code into single method GetData •	Removed old calls to webclient from Onget •	Removed using block from Onget

--- CAD_drug_report/Pages/Index.cshtml.cs
+++ CAD_drug_report/Pages/Index.cshtml.cs
@@ -20,43 +20,50 @@ namespace CAD_drug_report.Pages
 
         public void OnGet()
         {
-            //Web Client gives us access to data on the internet
+            //Drug Reports data
+            string drugData = GetData("https://data.cincinnati-oh.gov/resource/m3rc-s9gd.json"); 
+            List<QuickTypeDrug.Drug> alldrug = QuickTypeDrug.Drug.FromJson(drugData);
+
+            IDictionary<string, QuickTypeDrug.Drug> drugDictionary = new Dictionary<string, QuickTypeDrug.Drug>();
+                
+            foreach(QuickTypeDrug.Drug drug in alldrug)
+            {
+               drugDictionary.Add(drug.EventNumber, drug);
+            }
+
+            //CAD data
+            string jsonData = GetData("https://data.cincinnati-oh.gov/resource/qiik-bpks.json");
+              
+            List<QuickTypeCad.Cad> allcad = QuickTypeCad.Cad.FromJson(jsonData);
+
+            IList<QuickTypeCad.Cad> drugReport = new List<QuickTypeCad.Cad>();
+
+
+            foreach (QuickTypeCad.Cad cad in allcad)
+            {
+               Console.WriteLine(cad);
+
+               if(drugDictionary.ContainsKey(cad.EventNumber))
+               {
+                  drugReport.Add(cad);
+               }
+
+            }//end of foreach()
+
+            ViewData["allcad"] = drugReport;
+
+        }//end of using()
+
+        public string GetData(string endpoint)
+        {
+            string downloadedData = "";
             using (WebClient webClient = new WebClient())
             {
-               //Drug Reports data
-               string drugData = webClient.DownloadString("https://data.cincinnati-oh.gov/resource/m3rc-s9gd.json");
-
-               List<QuickTypeDrug.Drug> alldrug = QuickTypeDrug.Drug.FromJson(drugData);
-
-               IDictionary<string, QuickTypeDrug.Drug> drugDictionary = new Dictionary<string, QuickTypeDrug.Drug>();
-                
-                foreach(QuickTypeDrug.Drug drug in alldrug)
-                {
-                    drugDictionary.Add(drug.EventNumber, drug);
-                }
-
-               //CAD data
-               string jsonData = webClient.DownloadString("https://data.cincinnati-oh.gov/resource/qiik-bpks.json");
-              
-               List<QuickTypeCad.Cad> allcad = QuickTypeCad.Cad.FromJson(jsonData);
-
-               IList<QuickTypeCad.Cad> drugReport = new List<QuickTypeCad.Cad>();
-
-
-                foreach (QuickTypeCad.Cad cad in allcad)
-                {
-                    Console.WriteLine(cad);
-
-                    if(drugDictionary.ContainsKey(cad.EventNumber))
-                    {
-                        drugReport.Add(cad);
-                    }
-
-                }//end of foreach()
-
-                ViewData["allcad"] = drugReport;
-
-            }//end of using()
+                downloadedData = webClient.DownloadString(endpoint);
+            }
+            return downloadedData;
         }
+
     }
+
 }
