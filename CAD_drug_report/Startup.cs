ad7ce44e5cf122f524d38a5fdc070e20186be1b4
menonsn <54641774+menonsn@users.noreply.github.com>
2019-12-04 17:31:48 -0500

    Environment changes

--- CAD_drug_report/Startup.cs
+++ CAD_drug_report/Startup.cs
@@ -40,9 +40,9 @@ namespace CAD_drug_report
             }
             else
             {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseExceptionHandler("/Error");
+                //// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                //app.UseHsts();
             }
 
             app.UseHttpsRedirection();
